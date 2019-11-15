f05155c00b227af5744ae589770ef65000f8a89f
Ivan Valkou <zloyfey@gmail.com>
2019-11-15 13:12:51 +0300

    Fastlane

---
+++ fastlane/Fastfile.swift
@@ -0,0 +1,21 @@
+// This file contains the fastlane.tools configuration
+// You can find the documentation at https://docs.fastlane.tools
+//
+// For a list of all available actions, check out
+//
+//     https://docs.fastlane.tools/actions
+//
+
+import Foundation
+
+class Fastfile: LaneFile {
+	func betaLane() {
+	desc("Push a new beta build to TestFlight")
+        let project = "Loop/Loop.xcodeproj"
+        let buildNumber = numberOfCommits()
+
+        incrementVersionNumber(versionNumber: "2.0", xcodeproj: project)
+        incrementBuildNumber(buildNumber: buildNumber, xcodeproj: project)
+		uploadToTestflight(username: appleID, groups: ["Beta Users"])
+	}
+}
