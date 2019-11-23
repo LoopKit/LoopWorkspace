6426006ce66b403fada3b924ee31c80b0e2ac4a7
Ivan Valkou <zloyfey@gmail.com>
2019-11-23 17:56:12 +0300

    fastfile

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -10,13 +10,30 @@ import Foundation
 
 class Fastfile: LaneFile {
 	func betaLane() {
-	desc("Push a new beta build to TestFlight")
-        let project = "Loop/Loop.xcodeproj"
-        let buildNumber = numberOfCommits()
+        desc("Push a new beta build to TestFlight")
 
-        incrementVersionNumber(versionNumber: "2.0", xcodeproj: project)
+        let project = "Loop/Loop.xcodeproj"
+        let scheme = "Loop (Workspace)"
+        let groups = ["Loopers"]
+        let version = "2.0"
+        let buildNumber = numberOfCommits()
+        let changelog = """
+Повышена безопасность микроболюсов.
+
+Микроболюсы будут работать, если:
+- Текущее значение глюкозы выше порога остановки;
+- Предсказанное значение глюкозы выше порога остановки;
+- Текущее значение выше цели;
+- Предсказанное значение выше цели.
+
+На графики добавлена отметка текущего времени.
+
+Возможно(!) исправлен баг со слетающими настройками после перезагрузки телефона.
+"""
+
+        incrementVersionNumber(versionNumber: version, xcodeproj: project)
         incrementBuildNumber(buildNumber: buildNumber, xcodeproj: project)
-        buildApp(scheme: "Loop (Workspace)", clean: true)
-        uploadToTestflight(username: appleID, distributeExternal: true, groups: ["Loopers"], teamId: teamID)
+        buildApp(scheme: scheme, clean: true)
+        uploadToTestflight(username: appleID, changelog: changelog, distributeExternal: true, groups: groups, teamId: teamID)
 	}
 }
