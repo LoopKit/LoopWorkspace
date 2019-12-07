a6d78b4023a80833b149e230dbcbf2c35001b579
Ivan Valkou <zloyfey@gmail.com>
2019-12-07 14:49:37 +0300

    fastfile

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -9,28 +9,27 @@
 import Foundation
 
 class Fastfile: LaneFile {
-	func betaLane() {
-        desc("Push a new beta build to TestFlight")
+	func archiveLane() {
+        desc("Archeve")
 
         let project = "Loop/Loop.xcodeproj"
         let scheme = "Loop (Workspace)"
-        let groups = ["Loopers"]
         let version = "2.0"
         let buildNumber = numberOfCommits()
-        let changelog = """
-Переработан код микроболюсов. На их работе не отразилось, кроме следующего момента:
-    - Если текущая глюкоза ниже целевого диапазона, то микроболюсы ограничены 30 базальными минутами.
-
-ВАЖНО! Необходимо зайти на экран микроболюсов и заново их настроить!
-
-Исправлен баг с сохранением пустого Nightscout CGM.
-
-Исправлено открытие экрана микроболюсов при редактировании углеводов.
-"""
 
         incrementVersionNumber(versionNumber: version, xcodeproj: project)
         incrementBuildNumber(buildNumber: buildNumber, xcodeproj: project)
         buildApp(scheme: scheme, clean: true)
-        uploadToTestflight(username: appleID, changelog: changelog, distributeExternal: true, groups: groups, teamId: itcTeam)
 	}
+
+    func uploadLane() {
+        desc("Upload")
+
+        let groups = ["Loopers"]
+        let changelog =
+"""
+Changelog
+"""
+        uploadToTestflight(username: appleID, changelog: changelog, distributeExternal: true, groups: groups, teamId: itcTeam)
+    }
 }
