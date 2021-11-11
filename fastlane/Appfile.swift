c140c10e78036d7b008c43729d25427899d19e9a
marionbarker <marionbarker@earthlink.net>
2021-11-11 05:56:31 -0800

    remove fastlane

--- fastlane/Appfile.swift
+++
@@ -1,9 +0,0 @@
-var appIdentifier: String { return "com.BA7ZHP4963.loopkit.Loop" } // The bundle identifier of your app
-var appleID: String { return "ivalkou@gmail.com" } // Your Apple email address
-
-var itcTeam: String? { return "2082062" } // App Store Connect Team ID
-var teamID: String { return "BA7ZHP4963" } // Apple Developer Portal Team ID
-
-
-// For more information about the Appfile, see:
-//     https://docs.fastlane.tools/advanced/#appfile
