337cb6c04bf12f23edde3f743b7ec468cbc20771
Pete Schwamb <pete@schwamb.net>
2023-02-10 08:25:52 -0600

    Localization updates

--- Scripts/export_localizations.sh
+++ Scripts/export_localizations.sh
@@ -2,18 +2,21 @@
 
 LANGUAGES=(ar es ru en zh-Hans nl fr de it nb pl ru es ja pt-BR vi da sv fi ro tr he sk)
 
-xcodebuild -exportLocalizations -localizationPath xlocs "${LANGUAGES[@]/#/-exportLanguage }"
+argstring="${LANGUAGES[@]/#/-exportLanguage }"
+IFS=" "; args=( $=argstring )
 
-mkdir -p xliff
-find xclocs -name '*.xliff' -exec cp {} xliff \;
+xcodebuild -exportLocalizations -localizationPath xlocs $args
 
-cd xliff
-
-foreach lang in $LANGUAGES
-  echo lokalise2 \
-    --token $LOKALISE_TOKEN \
-    --project-id 8069387863cdd837d11dd0.82955128 \
-    file upload \
-    --file ${lang}.xliff \
-    --lang-iso ${lang}
-end
+#mkdir -p xliff
+#find xclocs -name '*.xliff' -exec cp {} xliff \;
+#
+#cd xliff
+#
+#foreach lang in $LANGUAGES
+#  echo lokalise2 \
+#    --token $LOKALISE_TOKEN \
+#    --project-id 8069387863cdd837d11dd0.82955128 \
+#    file upload \
+#    --file ${lang}.xliff \
+#    --lang-iso ${lang}
+#end
