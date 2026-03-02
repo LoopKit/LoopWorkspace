1a2f9ebbfd211e19ef701b39acb275311d24ffa6
marionbarker <marionbarker@earthlink.net>
2026-03-02 08:02:20 -0800

    add intro message to update_submodule_refs script

--- Scripts/update_submodule_refs.sh
+++ Scripts/update_submodule_refs.sh
@@ -2,13 +2,24 @@
 
 source Scripts/define_common.sh
 
+section_divider
+echo "You are running ${0}"
+echo
+echo "  This modifies your local clone, in whatever branch is currently selected,"
+echo "  so that every submodule is at the tip of the appropriate branch."
+echo
+current_branch=$(git branch --show-current 2>/dev/null)
+echo "  The current LoopWorkspace branch is $current_branch"
+
+continue_or_quit ${0}
+
 for project in ${PROJECTS}; do
+  echo
   echo "Updating to $project"
   IFS=":" read user dir branch <<< "$project"
   echo "Updating to $branch on $user/$project"
   cd $dir
   git checkout $branch
-  #git branch -D tidepool-sync
   git pull
   cd -
 done
