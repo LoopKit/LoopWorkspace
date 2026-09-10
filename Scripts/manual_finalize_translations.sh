665c0d6b3d346fbc69193af763d3a56e0b30214e
marionbarker <marionbarker@earthlink.net>
2026-09-10 15:10:35 -0700

    update Script to automate non-LoopKit translation PR

--- Scripts/manual_finalize_translations.sh
+++ Scripts/manual_finalize_translations.sh
@@ -16,28 +16,37 @@ next_script "./Scripts/manual_review_translations.sh"
 continue_or_quit ${0}
 
 LOOPKIT_USER="LoopKit"
+LOOPANDLEARN_USER="loopandlearn"
+LAL_REMOTE="lal"
 
 for project in ${PROJECTS}; do
     echo "Committing updates to $project"
     IFS=":" read user dir branch <<< "$project"
     cd $dir
-    git add .
+        git add .
         # skip repositories with no changes
         if git commit -F "../${MESSAGE_FILE}"; then
-            git push --set-upstream origin ${TRANSLATION_BRANCH}
-            # Only open PR if the owner is LoopKit
-            # the loopandlearn branch should be created or updated
-            #   then manually create the PR to the source repository
-            if [[ ${user} == ${LOOPKIT_USER} ]]; then
+            # push to origin for LoopKit and open the PR
+            if [[ ${user} == ${LOOPKIT_USER} || ${user} == ${LOOPANDLEARN_USER} ]]; then
+                git push --set-upstream origin ${TRANSLATION_BRANCH}
                 # If PR already exists, this just opens it
                 pr=$(gh pr create -B $branch -R $user/$dir --fill 2>&1 | grep http)
                 echo "PR = $pr"
                 open $pr
             else
-                echo "Automatic PR creation is only provided for LoopKit"
-                echo "  The branch ${TRANSLATION_BRANCH} was created or updated at $user/$dir"
-                echo "  Create the appropriate PR to the source repository"
-                echo "  After that PR is approved and merged, then sync $user/$dir"
+                # Ensure remote is configured
+                if ! git remote get-url ${LAL_REMOTE} &>/dev/null; then
+                    echo "   Adding remote ${LAL_REMOTE} for $dir"
+                    git remote add ${LAL_REMOTE} https://github.com/${LAL_GITHUB}/$dir
+                fi
+                # push the change to loopandlearn and then open the PR
+                echo "   Push this update to ${LOOPANDLEARN_USER}/$dir:${TRANSLATION_BRANCH}"
+                git push --set-upstream ${LAL_REMOTE} ${TRANSLATION_BRANCH}
+                echo ""
+                # If PR already exists, this just opens it
+                pr=$(gh pr create -B $branch -R ${LAL_REMOTE}/$dir --fill 2>&1 | grep http)
+                echo "PR = $pr"
+                open $pr
             fi
         fi
     cd -
