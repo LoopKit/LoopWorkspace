af15a3c9f968deea5ad3fdc2fd10d2b93a346a5a
marionbarker <marionbarker@earthlink.net>
2026-09-25 15:06:02 -0700

    update import localization Scripts to work with Xcode 27

--- Scripts/import_localizations.sh
+++ Scripts/import_localizations.sh
@@ -46,7 +46,7 @@ for project in ${PROJECTS}; do
 done
 
 # Build Loop
-set -o pipefail && time xcodebuild -workspace LoopWorkspace.xcworkspace -scheme 'LoopWorkspace' -destination 'generic/platform=iOS' build | xcpretty
+set -o pipefail && time xcodebuild -workspace LoopWorkspace.xcworkspace -scheme 'LoopWorkspace' -destination 'generic/platform=iOS' build | xcbeautify
 
 
 # Apply translations
